42bd2e6003440407484052ba00c2545968169b01
danielstretulch <joshuahappy@live.com>
2019-07-11 08:24:26 -0400

    Add Seeds to Match.js

--- Match.js
+++ Match.js
@@ -12,6 +12,10 @@ var currentMatch = [];
 var confirmButton;
 
 function startGame(){
+	if(seed != null){
+		randomSeed(seed);
+	}
+	
 	for(var i = 0; i < playerOneChars.length; i++){
 		playerOneTeam.push(playerOneChars[i].value());
 	}
